e8fdebe1b299045d064f8ca51379491c12173c76
042annyxx <159548777+042annyxx@users.noreply.github.com>
2024-09-26 12:20:47 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,155 @@
+criaCartao(
+    'shadowhunters',
+    'O que são Shadowhunters?',
+    'Shadowhunters são humanos com sangue de anjo que caçam demônios e protegem o mundo dos seres sobrenaturais.'
+)
+
+criaCartao(
+    'shadowhunters',
+    'Quem é o autor da série "Os Instrumentos Mortais"?',
+    'Cassandra Clare.'
+)
+
+criaCartao(
+    'shadowhunters',
+    'Qual é o nome da protagonista da série?',
+    'Clary Fray.'
+)
+
+criaCartao(
+    'shadowhunters',
+    'Quem é o melhor amigo de Clary?',
+    'Simon Lewis.'
+)
+criaCartao(
+    'shadowhunters',
+    'Qual é a relação entre Clary e Jace?',
+    'Eles têm uma ligação romântica complexa, que evolui ao longo da série.'
+)
+criaCartao(
+    'shadowhunters',
+    'Quem é Magnus Bane?',
+    'magnus é um feiticeiro poderoso e carismático que se torna um aliado dos Shadowhunters.'
+)
+criaCartao(
+    'shadowhunters',
+    'Qual é o nome da irmandade de Shadowhunters que Clary se junta?',
+    'o instituto de nova york'
+)
+criaCartao(
+    'shadowhunters',
+    'Quem é Valentine Morgenstern?',
+    'Valentine é o principal antagonista da série, um ex-Shadowhunter que busca eliminar os seres sobrenaturais.'
+)
+criaCartao(
+    'shadowhunters ',
+    'Qual é o nome do livro que inicia a série "Os Instrumentos Mortais"',
+    'Cidade dos Ossos'
+)
+criaCartao(
+    'shadowhunters',
+    'Quem é o personagem que se torna o líder do Instituto após a morte de seu pai?',
+    'Alec Lightwood.'
+)
+criaCartao(
+    'shadowhunters',
+    'Qual é o papel de Hodge Starkweather na história?',
+    'Hodge é um mentor e professor de Shadowhunters, que tem um passado complicado.'
+)
+criaCartao(
+    'shadowhunters',
+    'Qual é o nome da cidade onde os eventos principais da série acontecem',
+    'nova york'
+)
+criaCartao(
+    'shadowhuntes',
+    'Quem é o pai biológico de Jace?',
+    'Valentine Morgenstern.'
+)
+criaCartao(
+    'shadowhunters',
+    'Qual é o nome da organização de demônios que aparece na série?',
+    'cla de fadas e o de vampiros'
+)
+criaCartao(
+    'shadowhunters',
+    'Quem é a loba que se torna amiga de Clary?',
+    'maia roberts'
+)
+criaCartao(
+    'shadowhunters',
+    'Qual é a função dos Caçadores de Sombras?',
+    'matar demonios para proteger os humanos'
+)
+criaCartao(
+    'shadowhunters',
+    'O que acontece com a mãe de Clary no início da série?',
+    'foi seuqestrada por valentine'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'shadowhuters',
+    'Como Clary descobre que é uma Caçadora de Sombras?',
+    'Quando ela vê um demônio em um clube e é levada para o mundo dos Shadowhunters.'
+)
+criaCartao(
+    'shadowhunters',
+    'Quem é a personagem que se transforma em uma fada?',
+    'Aline Penhallow'
+)
+criaCartao(
+    'shadowhunters',
+    'Qual é a importância da Capa do Inimigo?',
+    'É um artefato que permite aos Shadowhunters se esconderem de seres sobrenaturais.'
+)
+criaCartao(
+    'shadowhunters',
+    'Quem é o personagem que representa a conexão entre os Shadowhunters e os seres sobrenaturais?',
+    'magnus bane'
+)
+criaCartao(
+    'shadowhunters',
+    'Qual é a mensagem central da série "Shadowhunters"?',
+    'A série aborda temas como amor, amizade, identidade e aceitação entre diferentes mundos e culturas.'
+)
+
